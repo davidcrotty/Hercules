f456a43e3b80fcb46cab5fdddf706eaf0b545daf
David Crotty <david.crotty.work@gmail.com>
2017-09-15 21:43:25 +0100

    Adds 'ExerciseActivity'

---
+++ app/src/main/java/net/davidcrotty/hercules/ExerciseActivity.kt
@@ -0,0 +1,16 @@
+package net.davidcrotty.hercules
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+
+/**
+ * Created by David Crotty on 15/09/2017.
+ *
+ * Copyright © 2017 David Crotty - All Rights Reserved
+ */
+class ExerciseActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_exercise)
+    }
+}
