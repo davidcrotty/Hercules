d60c834b2a3abdc717fe031c2564602a4ad8d785
David Crotty <david.crotty.work@gmail.com>
2017-09-16 14:58:42 +0100

    Scaffold for progress ring drawable

--- app/src/main/java/net/davidcrotty/hercules/ExerciseActivity.kt
+++ app/src/main/java/net/davidcrotty/hercules/ExerciseActivity.kt
@@ -2,6 +2,7 @@ package net.davidcrotty.hercules
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_exercise.*
 
 /**
  * Created by David Crotty on 15/09/2017.
@@ -16,5 +17,8 @@ class ExerciseActivity : AppCompatActivity() {
         supportActionbar?.setDisplayHomeAsUpEnabled(true)
         supportActionbar?.setDefaultDisplayHomeAsUpEnabled(true)
         supportActionbar?.title = ""
+
+        progress_countdown.max = 10
+        progress_countdown.progress = 4
     }
 }
